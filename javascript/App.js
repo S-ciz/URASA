4c68aa8c3a3bda0ab5e72b7511a97312afa0d0e7
S-ciz <95995178+S-ciz@users.noreply.github.com>
2024-03-20 07:03:08 +0200

    Add files via upload

---
+++ javascript/App.js
@@ -0,0 +1,23 @@
+//Features
+
+import health from "../img/doctor.jpg"
+const Features = [ 
+  {
+    id: 0,
+    img: health,
+    title: "Education",
+    text: "at URASA we offer health promotion programs to refugees through health education, grassroot awareness programs, advocacy campaigns, seminars and workshops with refugee-led organizations (RLOs) representatives as well as to refugees and asylum seekers directly. "
+  },
+  {
+    id: 1,
+    img: health,
+    title: "Healthcare",
+    text: "at URASA we offer health promotion programs to refugees through health education, grassroot awareness programs, advocacy campaigns, seminars and workshops with refugee-led organizations (RLOs) representatives as well as to refugees and asylum seekers directly. "
+  },
+  {
+    id: 0,
+    img: health,
+    title: "Healthcare",
+    text: "at URASA we offer health promotion programs to refugees through health education, grassroot awareness programs, advocacy campaigns, seminars and workshops with refugee-led organizations (RLOs) representatives as well as to refugees and asylum seekers directly. "
+  },
+]
